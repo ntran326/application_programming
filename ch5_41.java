c2bdffa2b3def4d3258364f4e51a3c61211fbae5
Nhu Tran <nhutran326@gmail.com>
2022-10-13 13:55:08 -0400

    new changes

--- ch5_41.java
+++ ch5_41.java
@@ -1,5 +1,30 @@
 // Nhu Tran
 
+import java.util.Scanner;
+
 public class ch5_41 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+		System.out.print("Enter numbers: ");
+		int num = input.nextInt();
+        int max = 0;
+		int count = 1;	
+
+
+		while(num != 0){
+            if(num > max){
+                count = 1;
+                max = num;
+            }
+
+            else if (max == num)
+                count++;
+                num = input.nextInt();
+        }
+
+            System.out.println("The largest number is " + max);
+            System.out.println("The occurrence count of the largest number is " + count);
+    }
 }
--- ch6_23.java
+++ ch6_23.java
@@ -1,3 +1,34 @@
+// Nhu Tran
+
+import java.util.Scanner;
+
 public class ch6_23 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a string followed by a character: ");
+		String string = input.nextLine();
+
+
+		int k = string.indexOf(", ");
+		String str = string.substring(0, k);
+		char ch = string.charAt(k + 2);
+
+
+		System.out.println("The number of occurrences of \"" + ch + "\" in \'" + str + "\" is: " +
+			count(str, ch));
+	}
+
+
+	public static int count(String str, char a) {
+		int count = 0; 
+
+		for (int i = 0; i < str.length(); i++) {
+			if (a == str.charAt(i))
+				count++;
+		}
+
+		return count;
+	}
 }
+
